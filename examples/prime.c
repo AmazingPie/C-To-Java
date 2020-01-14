ec17ecae876db6b91de7ca3e246d9518beec321d
AmazingPie <cjrulesgotit@gmail.com>
2020-01-14 13:22:38 +0000

    Added new example, prime.c

---
+++ examples/prime.c
@@ -0,0 +1,30 @@
+//#include <stdio.h>
+
+int isPrime(int n) {
+	int i,m=0,flag=0;
+	m=n/2;
+	for(i=2;i<=m;i++) {
+		if(n%i==0) {
+			flag=1;
+			return 0;
+		}
+	}
+	if(flag==0) {
+		return 1;
+	}
+	return 0;
+}
+/*
+int main() {
+	printf("Enter the number to check prime:");
+        int n = 0;
+	scanf("%d",&n);
+	if (isPrime(n) == 1) {
+		printf("Number is prime\n");
+	}
+	else {
+		printf("Number is not prime\n");
+	}
+	return 0;
+}
+*/
